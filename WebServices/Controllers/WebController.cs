d72c19947f1620160cc034994d6a2ec63fc02c89
unknown <NH229U06@NH229C06.csd.bit>
2017-04-24 21:46:10 -0500

    Initial Commit

---
+++ WebServices/Controllers/WebController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using WebServices.Models;
+
+namespace WebServices.Controllers
+{
+    public class WebController : ApiController
+    {
+        private ReservationRepository repo = ReservationRepository.Current;
+
+        public IEnumerable<Reservation> GetAllReservations()
+        {
+            return repo.GetAll();
+        }
+
+        public Reservation PostReservation(Reservation item)
+        {
+            return repo.Add(item);
+        }
+
+        public bool PutReservation(Reservation item)
+        {
+            return repo.Update(item);
+        }
+
+        public void DeleteReservation(int id)
+        {
+            repo.Remove(id);
+        }
+    }
+}
